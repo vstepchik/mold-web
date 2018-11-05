69b8ceae905ca6f151f3aa3966814cb497bd3c94
Vitalik Stepchik <vitalik.stepchik@omnixlabs.com>
2018-11-06 01:42:14 +0200

    listing articles on index

--- src/markup/index.rs
+++ src/markup/index.rs
@@ -1,8 +1,18 @@
+use crate::markup::ARTICLES;
 use crate::markup::base::template_base;
 use maud::{html, Markup};
+
+const ARTICLE_URL_PREFIX: &'static str = "/a";
 
 pub fn index() -> Markup {
     template_base("Home", html! {
         p { "Hello" }
+        ul {
+            @for (id, _article) in ARTICLES.entries() {
+                li {
+                    a href={ (ARTICLE_URL_PREFIX) "/" (id) } { (id) }
+                }
+            }
+        }
     })
 }
