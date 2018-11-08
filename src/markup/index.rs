c2de289945a4d80771c17ed702fb24734b65152b
Vitalik Stepchik <vitalik.stepchik@omnixlabs.com>
2018-11-09 00:00:47 +0200

    removed the hello word

--- src/markup/index.rs
+++ src/markup/index.rs
@@ -7,7 +7,6 @@ const ARTICLE_URL_PREFIX: &'static str = "/a";
 
 pub fn index(is_night: bool) -> Markup {
     template_base(is_night, "Home", html! {
-        h1 { "Hello" }
         @for (id, article) in ARTICLES.entries().rev() {
             div.article {
                 h3 { a href={ (ARTICLE_URL_PREFIX) "/" (id) } { (article.title()) } }
