24c5968da1c10313a13970da8ae73548b513c835
Vitalik Stepchik <vitalik.stepchik@omnixlabs.com>
2018-12-05 11:47:53 +0200

    added meta keywords and description to the index page

--- src/markup/index.rs
+++ src/markup/index.rs
@@ -6,15 +6,21 @@ use crate::markup::base::template_base;
 const ARTICLE_URL_PREFIX: &'static str = "/a";
 
 pub fn index(is_night: bool) -> Markup {
-    template_base(is_night, "Mildew", None, html! {
-        @for (id, article) in ARTICLES.entries().rev() {
-            div.article {
-                h3 { a href={ (ARTICLE_URL_PREFIX) "/" (id) } { (article.title) } }
-                p {
-                    ((article.summary)())
-                    span.date { (article.date) }
+    template_base(
+        is_night, "Mildew",
+        Some(html! {
+            meta name="description" content="A personal blog about procedural content generation and process simulation.";
+            meta name="keywords" content="vitalatron,vstepchik,personal blog,blog,procedural content generation,procedural content,generator,simulation,process simulation,python,kotlin,rust,mold,mildew,mushroom";
+        }),
+        html! {
+            @for (id, article) in ARTICLES.entries().rev() {
+                div.article {
+                    h3 { a href={ (ARTICLE_URL_PREFIX) "/" (id) } { (article.title) } }
+                    p {
+                        ((article.summary)())
+                        span.date { (article.date) }
+                    }
                 }
             }
-        }
-    })
+        })
 }
