08048344a7c16d17e67b254646b4dedf797b7a99
Vitalik Stepchik <vitalik.stepchik@omnixlabs.com>
2018-12-13 10:49:35 +0200

    ssl

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 #![feature(plugin, proc_macro_hygiene)]
 #![plugin(phf_macros)]
 
+extern crate actix;
+extern crate actix_web;
 extern crate bytes;
 extern crate env_logger;
 extern crate includedir;
 extern crate maud;
 extern crate mime_guess;
 extern crate phf;
+extern crate rustls;
+
+use std::fs::File;
+use std::io::BufReader;
 
 use actix_web::{App, http, HttpRequest, HttpResponse, Result, server};
 use actix_web::http::header;
 use actix_web::middleware::{ErrorHandlers, Logger, Response};
 use bytes::Bytes;
 use maud::Markup;
+use rustls::{NoClientAuth, ServerConfig};
+use rustls::internal::pemfile::{certs, rsa_private_keys};
 
 use crate::markup::ARTICLES;
 use crate::static_res::StaticResource;
@@ -66,15 +74,29 @@ fn create_app() -> App {
 
 fn main() {
     const LOG_ENV_VAR: &str = "RUST_LOG";
-    let socket = "0.0.0.0:8000";
+    let socket = "0.0.0.0:8443";
 
     if std::env::var_os(LOG_ENV_VAR).is_none() {
         std::env::set_var(LOG_ENV_VAR, "info");
     }
     env_logger::init();
 
+    // load ssl keys
+    let mut config = ServerConfig::new(NoClientAuth::new());
+    let cert_file = &mut BufReader::new(File::open("cert.pem").expect("cert.pem not found"));
+    let key_file = &mut BufReader::new(File::open("key.pem").expect("key.pem not found"));
+    let cert_chain = certs(cert_file).expect("failed to read cert file");
+    let mut keys = rsa_private_keys(key_file).expect("failed to read keys file");
+    config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
+
+    // actix acceptor
+    let acceptor = server::RustlsAcceptor::with_flags(
+        config,
+        server::ServerFlags::HTTP1 | server::ServerFlags::HTTP2,
+    );
+
     server::new(|| create_app())
-        .bind(socket)
+        .bind_width(socket, move || acceptor.clone())
         .expect("Unable to bind socket")
         .keep_alive(10)
         .run();
