d7252a6725b62d3e50b22be920b43f5c1e1cc0f2
Vitalii Stepchyk <vitalatron@gmail.com>
2022-09-02 18:09:06 +0200

    somehow fix robots.txt and favicon

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use actix_web::middleware::{Compress, Logger};
 use actix_web::{web, App, HttpServer};
 use actix_web_middleware_redirect_scheme::RedirectSchemeBuilder;
 use actix_web_static_files::ResourceFiles;
+use std::collections::HashMap;
 use std::env;
 
 mod cookies;
@@ -33,6 +34,13 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         let generated = generate();
+        let mut files_at_root: HashMap<&str, static_files::Resource> = HashMap::new();
+        let robots = generated.get("robots.txt").unwrap();
+        let robots = static_files::resource::new_resource(robots.data, robots.modified, robots.mime_type);
+        let favicon = generated.get("favicon.ico").unwrap();
+        let favicon = static_files::resource::new_resource(favicon.data, favicon.modified, favicon.mime_type);
+        files_at_root.insert("robots.txt", robots);
+        files_at_root.insert("favicon.ico", favicon); // todo: fix it the proper way
         App::new()
             .wrap(Logger::default())
             .wrap(RedirectSchemeBuilder::new().enable(!disable_https).build()) // redirect HTTP -> HTTPS
@@ -40,6 +48,7 @@ async fn main() -> std::io::Result<()> {
             .route("/", web::get().to(markup::index))
             .route("/a/{article_id}", web::get().to(markup::article))
             .service(ResourceFiles::new("/s", generated).do_not_resolve_defaults())
+            .service(ResourceFiles::new("/", files_at_root).do_not_resolve_defaults())
             .default_service(web::route().to(markup::e404))
     })
     .bind(("0.0.0.0", port))?
